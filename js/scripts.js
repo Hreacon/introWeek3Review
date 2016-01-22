9fc44b6fef1fb664e2449f1b5814c53ee208325f
Nicholas Jensen <nicholasjensenhay@gmail.com>
2016-01-22 11:25:13 -0800

    update pingPong to handle strings with numbers in front of characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function pingPong(number) {
   var output = parseInt(number);
-  if(number%15 === 0)
+  if( output%15 === 0 )
     output = 'pingpong';
-  else if(number%3 === 0)
+  else if( output%3 === 0 )
     output = 'ping';
-  else if( number%5 === 0 )
+  else if( output%5 === 0 )
     output = 'pong';
   return output;
 }
