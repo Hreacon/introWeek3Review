3d27d2a6433cd55399ea0d1bbb45a77d0638c7f8
Nicholas Jensen <nicholasjensenhay@gmail.com>
2016-01-22 10:29:58 -0800

    write spec for numbers divisible by 3, return ping

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('pingPong', function() {
   it('will return the number provided', function() {
     expect(pingPong(1)).to.equal(1);
   });
+  it('will return ping when given numbers divisible by 3', function() {
+    expect(pingPong(6)).to.equal('ping');
+  })
 });
