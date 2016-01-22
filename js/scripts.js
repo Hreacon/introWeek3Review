4737cb9719d3ad53ada2f6ca6b951a32c140b8c7
Nicholas Jensen <nicholasjensenhay@gmail.com>
2016-01-22 10:39:48 -0800

    update pingPong to return pinpong when given numbers divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function pingPong(number) {
   var output = number;
-  if(number%3 === 0)
+  if(number%15 === 0)
+    output = 'pingpong';
+  else if(number%3 === 0)
     output = 'ping';
   else if( number%5 === 0 )
     output = 'pong';
